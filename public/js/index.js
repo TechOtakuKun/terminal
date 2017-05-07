7411638d3acc8fdab43c56e874ec55ca828d5b54
Zenaro <572001239@qq.com>
2017-05-07 19:53:07 +0800

    quote

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,9 @@
 			inputString: ''
 		},
 		ready: function() {
-			this.splitString("234.324 + 5");
+			// this.splitString("234.324 + 5");
+			var string = "'123'456";
+			console.log(this.isValuate(string));
 		},
 		methods: {
 
@@ -21,62 +23,110 @@
 			 */
 			pushData: function() {
 				var prevList = this.stringList;
+				var originString = this.inputString;
+				this.splitString(originString);
+				var resultString = this.formatStack();
+
+				// 将计算结果添加进视图数组中
 				prevList.push({
-					stringOrigin: this.inputString,
-					stringResult: this.inputString,
+					stringOrigin: originString,
+					stringResult: resultString,
 					type: 'success'
 				});
-				this.$set('stringList', prevList);
-				this.$set('inputString', '');
+				this.$set('stringList', prevList); // 更新数据结构stringList
+				this.$set('inputString', ''); // 还原input控件的内容
 			},
 
 			/*
-			 * @param 输入的字符串
-			 * 对输入的字符串进行切割
+			 * 分析输入的字符串
+			 * 1. 为直接的运算语句，如 3 + 5
+			 * 2. 还是赋值语句，如 c = 3 + 5
+			 * return true:赋值语句 、 false: 直接的运算语句
+			 */
+			isValuate: function(string) {
+				var quoteReg = /(\'.*\'|\".*\")/g; // 匹配引号中的内容
+				// 判断字符串是否为赋值语句，需去除引号，再确认"="号
+				string = string.replace(quoteReg, "");
+				if (string.indexOf("=") > 0) {
+					return true;
+				} else {
+					return false;
+				}
+			},
+
+			/*
+			 * @param string 可运算的字符串
+			 * 对给定字符串进行切割
 			 * 切割的同时分析字符串是否合法
-			 * 切割后压入对应的栈
-			 * 变量或数字压入numStack，运算符压入optStack
+			 * 切割后分析，并压入对应的栈
+			 * 变量或数字压入this.numStack，运算符压入this.optStack
 			 * return
 			 */
 			splitString: function(string) {
 				var blankReg = /(^\s*)|(\s*$)/g; // 匹配首尾空格
 				var optReg = /(^[\+\-\*\/\!\%\^\&\(\)])/; // 匹配运算符+-*/!%&左右括号
-				var numReg = /(^\d+)(\.?)(\d*)/; // 匹配数字（包括int、double） 
+				var numReg = /(^\d+|\-\d+)(\.?)(\d*)/; // 匹配数字（包括正负、int、double） 
 				var varReg = /(^\w+)/; // 匹配变量 
-				var optStack = []; // 运算符栈
-				var numStack = []; // 数字栈
-				var validSymbol = ''; // 合法的字符
+				var target = ""; // 匹配的字符串
+				var validSymbol = 'identifier'; // 下一个后续合法的字符类型
+
 				while (string.length > 0) {
 
 					string = string.replace(blankReg, ''); // 去除空格
+					if (validSymbol == 'identifier') { // 下一个合法字符为数字或变量
+						if (numReg.test(string)) { // 数字
+							target = parseFloat(string.match(numReg)[0]);
+							string = string.replace(numReg, "");
 
-					if (optReg.test(string)) { // 匹配结果为运算符
-						optStack.push(string[0]);
+						} else if (varReg.test(string)) { // 变量
+							target = string.match(varReg)[0];
+							string = string.replace(varReg, "");
+						}
+						this.numStack.push(target);
+						validSymbol = 'opt';
+
+					} else if (validSymbol == 'opt') { // 下一个合法字符为运算符
+						if (optReg.test(string)) { // 匹配结果为运算符
+							this.optStack.push(string[0]);
+							string = string.substring(1);
+						}
+						validSymbol = 'identifier';
+					}
+
+					/*if (optReg.test(string)) { // 匹配结果为运算符
+						this.optStack.push(string[0]);
 						string = string.substring(1);
 
 					} else if (numReg.test(string)) { // 数字
 						var temp = parseFloat(string.match(numReg)[0]);
-						numStack.push(temp);
+						this.numStack.push(temp);
 						string = string.replace(numReg, "");
 
 					} else if (varReg.test(string)) { // 变量
 						var temp = string.match(varReg)[0];
-						numStack.push(temp);
+						this.numStack.push(temp);
 						string = string.replace(varReg, "");
 
 					} else {
 						console.log("ERROR - 无法处理的符号：" + string[0] + "(来自" + string + ")");
 						break;
-					}
+					}*/
 				}
-				console.log(this.calculate(optStack.pop(), numStack.pop(), numStack.pop()));
 			},
 
 			/*
-			 * 栈的计算
+			 * 对optStack和numStack两个栈进行计算
 			 */
 			formatStack: function() {
-				t
+				var optStack = this.optStack;
+				var numStack = this.numStack;
+				// 循环地弹出栈并进行计算
+				while (optStack.length > 0) {
+					var temp = this.calculate(optStack.pop(), numStack.pop(), numStack.pop());
+					numStack.push(temp);
+				}
+				// 当optStack为空时，numStack剩下唯一一个元素，即numStack[0]即为结果
+				return numStack.pop();
 			},
 
 			/*
